86368174e48186e2e5b73c8e93086bee89704f8c
nicky goodman <nickyggoodman@gmail.com>
2024-09-13 09:28:12 -0400

    adding script to chat

---
+++ resources/scripts/speaking.js
@@ -0,0 +1,33 @@
+/*
+see the python source code here: 
+https://github.com/VaasuDevanS/cowsay-python/blob/main/cowsay/main.py
+
+ _________________________________________
+/ Greetings! My name is Nicholas. I       \
+| graduated from the University of        |
+| Maryland, College Park in May 2024 with |
+| dual degrees in Computer Science and    |
+| Economics. Some of my interests are     |
+| software, aquariums, design, jazz,      |
+| nature, biking, and systems.            |
+| Originally from Frederick, Maryland, I  |
+| have been residing in Baltimore for the |
+| past three years. For further details   |
+| about my professional experience,       |
+\ please refer to my resume               /
+ -----------------------------------------          
+    \
+     \
+          .--.
+         |o_o |
+         |:_/ |
+        //   \ \
+       (|     | )
+      /'\_   _/`\
+      \___)=(___/
+
+*/
+
+function draw() {
+    // draw here
+}
