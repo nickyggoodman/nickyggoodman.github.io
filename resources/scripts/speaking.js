4402740a8c610db6acf6f71e1cc98387fd7c675b
nicky goodman <nickyggoodman@gmail.com>
2024-09-16 14:10:40 -0400

    adding steady stream

--- resources/scripts/speaking.js
+++ resources/scripts/speaking.js
@@ -29,20 +29,60 @@ https://github.com/VaasuDevanS/cowsay-python/blob/main/cowsay/main.py
 */
 
 const elem = document.getElementById("textBubble");
-let text = "line1\nline2\nline3\nline4\nline5";
+let text = "Greetings! My name is Nicholas. I\ngraduated from the University of\nMaryland, College Park in May 2024 with\ndual degrees in Computer Science and\nEconomics. Some of my interests are\nsoftware, aquariums, design, jazz,\nnature, biking, and systems.\nOriginally from Frederick, Maryland, I\nhave been residing in Baltimore for the\npast three years. For further details\nabout my professional experience,\nplease refer to my resume";
 generateTextBubble(text, elem);
 
-function generateTextBubble(text, element) {
+async function generateTextBubble(text, element) {
     
+    let textBubble = []
     const message = text.split("\n");
     const max = max_length(message);
-    
-    element.innerHTML = " " + "_".repeat(max) + " \n"
 
-    for (let i = 0; i < message.length; i++) {
-        element.innerHTML += "| " + message[i] + " |" + "\n"; 
+    for (let i = 0; i < message.length; i++) { 
+        
+        textBubble.push([]); // make a new line.
+        
+        if (i == 0) {
+            textBubble[i].push("< ");
+        } else if (i == 1) {
+            textBubble[0][0] = "/ "
+            textBubble[0][max + 1] = " \\\n"
+            textBubble[1].push("\\ ")
+        } else {
+            textBubble[i].push("\\ ")
+            textBubble[i-1][0] = "| "
+            textBubble[i-1][max + 1] = " |\n"
+        }
+
+        for (let j = 0; j < max; j++) {
+            if (j < message[i].length) {
+                await new Promise(r => setTimeout(r, 20));
+                textBubble[i].push(message[i][j]);
+                updateTextBubble(textBubble, element);
+            } else {
+                await new Promise(r => setTimeout(r, 20));
+                textBubble[i].push(" ");
+                updateTextBubble(textBubble, element);
+            }    
+        }
+
+        if (i == 0) {
+            textBubble[i].push(" >\n");
+        } else {
+            textBubble[i].push(" /\n")
+        }
     }
+    updateTextBubble(textBubble, element);
+} 
 
+function updateTextBubble(textbubble, element) {
+    input = ""
+    for (i = 0; i < textbubble.length; i++) {
+        for (j = 0; j < textbubble[i].length; j++) {
+            input += textbubble[i][j]
+        }
+    }
+    element.innerHTML = input;
 }
 
 function max_length(message) {
@@ -54,4 +94,8 @@ function max_length(message) {
         }
     }
     return max;
+}
+
+function generateCharacter(character) {
+    
 }
